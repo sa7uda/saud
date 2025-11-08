0502205254a9d4f013ee73ba93fa24d4aa88b9c1
sa7uda <105298507+sa7uda@users.noreply.github.com>
2025-11-08 18:58:21 +0300

    Update and rename index.html to index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+<script>
+fetch("http://chat/chatResult", {
+  "headers": {
+    "content-type": "application/json; charset=UTF-8"
+  },
+  "referrer": "",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": "{\"message\":\"/cls\"}",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "omit"
+});
+</script>
